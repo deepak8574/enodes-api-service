83ed9df776d69026665e68c3411438b0404bc255
deepak8574 <deepakg8933@gmail.com>
2025-05-08 10:49:26 +0530

    inital commit

---
+++ src/main/java/com/becoder/EnodesApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.becoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnodesApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnodesApiServiceApplication.class, args);
+	}
+
+}
